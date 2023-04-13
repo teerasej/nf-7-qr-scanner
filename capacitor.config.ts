9722106c7ab60b0ca4fcf36b97600ec0ad9362cd
teerasej <teerasej@gmail.com>
2023-04-13 18:45:04 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'NF7 QR Scanner',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
