30acf3941f7eb7aa7a4d7f1a52bcc5a9981987de
teerasej <teerasej@gmail.com>
2023-04-15 12:55:25 +0700

    add button to activate permission

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,24 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner' 
+
 const Home: React.FC = () => {
+
+  const onStartScan = async () => {
+    await BarcodeScanner.checkPermission({force: true})
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Nextflow Scanner</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonButton onClick={onStartScan}>Scan</IonButton>
       </IonContent>
     </IonPage>
   );
